fba4e5e855bb07cf57c34008e5bf27088225fee8
Shir Somech <shirs13@meet.mit.edu>
2014-04-28 18:43:35 +0300

    frosting

--- main.py
+++ main.py
@@ -4,6 +4,61 @@ import Box
 def erase(color):
 	main_screen.fill(color)
 
+def fro():
+
+	one = Box.Box(37.5, 150, 100, 100, 64, 224, 208)
+	Box.Box.draw(one, main_screen)
+	one.drawimg("Frostings/frosting1.jpg", main_screen)
+
+	two = Box.Box(170, 150, 100, 100, 64, 224, 208)				
+	Box.Box.draw(two, main_screen)
+	two.drawimg("Frostings/frosting2.jpg", main_screen)
+
+	three = Box.Box(302.5, 150, 100, 100, 64, 224, 208)
+	Box.Box.draw(three, main_screen)
+	three.drawimg("Frostings/frosting3.jpg", main_screen)
+
+	four = Box.Box(37.5, 337.5, 100, 100, 64, 224, 208)
+	Box.Box.draw(four, main_screen)
+	four.drawimg("Frostings/frosting4.jpg", main_screen)
+				
+	five = Box.Box(170, 337.5, 100, 100, 64, 224, 208)
+	Box.Box.draw(five, main_screen)
+	five.drawimg("Frostings/cs.jpg", main_screen)
+
+	six = Box.Box(302.5, 337.5, 100, 100, 64, 224, 208)
+	Box.Box.draw(six, main_screen)
+	six.drawimg("Frostings/cs.jpg", main_screen)
+	
+	#if one.rec.collidepoint(x,y):
+	#	top()
+	#if two.rec.collidepoint(x,y):
+	#	top()
+	#if three.rec.collidepoint(x,y):
+	#	top()
+	#if four.rec.collidepoint(x,y):
+	#	top()
+
+def top():
+	one = Box.Box(37.5, 150, 100, 100, 64, 224, 208)
+	Box.Box.draw(one, main_screen)
+	#one.drawimg("Actual", main_screen)
+
+	two = Box.Box(170, 150, 100, 100, 64, 224, 208)				
+	Box.Box.draw(two, main_screen)
+	#two.drawimg("Frostings/5_single_cupcake_crop.jpg", main_screen)
+
+	three = Box.Box(302.5, 150, 100, 100, 64, 224, 208)
+	Box.Box.draw(three, main_screen)
+	#three.drawimg("Frostings/frosting1.jpg", main_screen)
+
+	four = Box.Box(37.5, 337.5, 100, 100, 64, 224, 208)
+	Box.Box.draw(four, main_screen)
+	five = Box.Box(170, 337.5, 100, 100, 64, 224, 208)
+	Box.Box.draw(five, main_screen)
+	six = Box.Box(302.5, 337.5, 100, 100, 64, 224, 208)
+	Box.Box.draw(six, main_screen)
+
 if __name__=="__main__":
 #start pygame
 	pygame.init()
@@ -22,23 +77,32 @@ if __name__=="__main__":
 				
 				one = Box.Box(37.5, 150, 100, 100, 64, 224, 208)
 				Box.Box.draw(one, main_screen)
-				one.drawimg("Frostings/Gluten-free-chocolate-Nutella-cupcakes-close-up-21.jpg", main_screen)
+				one.drawimg("Actual/a1.jpg", main_screen)
 
 				two = Box.Box(170, 150, 100, 100, 64, 224, 208)				
 				Box.Box.draw(two, main_screen)
-				two.drawimg("Frostings/5_single_cupcake_crop.jpg", main_screen)
+				#two.drawimg("Frostings/5_single_cupcake_crop.jpg", main_screen)
 
-				Box.Box.draw(two, main_screen)
 				three = Box.Box(302.5, 150, 100, 100, 64, 224, 208)
 				Box.Box.draw(three, main_screen)
+				#three.drawimg("Frostings/frosting1.jpg", main_screen)
+
 				four = Box.Box(37.5, 337.5, 100, 100, 64, 224, 208)
 				Box.Box.draw(four, main_screen)
 				five = Box.Box(170, 337.5, 100, 100, 64, 224, 208)
 				Box.Box.draw(five, main_screen)
 				six = Box.Box(302.5, 337.5, 100, 100, 64, 224, 208)
 				Box.Box.draw(six, main_screen)
+
+
 			if one.rec.collidepoint(x,y):
-				print "hi"
+				fro()
+			if two.rec.collidepoint(x,y):
+				fro()
+			if three.rec.collidepoint(x,y):
+				fro()
+			if four.rec.collidepoint(x,y):
+				fro()
 		
 		pygame.display.flip()
 
